526c756c5008d85dde2cef8fe5bb92a1aca74e35
ahoodatheguy <ahadukebede@gmail.com>
2021-11-26 08:59:09 -0500

    Removed cybervim as it's better managed by manually cloning it

--- cybervim/lua/appearance/theme.lua
+++
@@ -1,8 +0,0 @@
--- Fix Pink Completion issue
-vim.cmd[[
-hi Pmenu ctermbg=NONE ctermfg=NONE
-hi PmenuSel ctermbg=NONE ctermfg=NONE
-]]
-
-vim.g.vscode_style = "dark"
-vim.cmd('colorscheme vscode')
--- cybervim/lua/code/general.lua
+++
@@ -1,4 +0,0 @@
-vim.o.expandtab = false
-vim.cmd('autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o') -- Disable Autocomment
-vim.opt.tabstop = 4
-vim.opt.shiftwidth= 4
